ebc54fc2dbb40012460d32e8b6ac447e000b34ef
javajaewook <leejaewook133@gmail.com>
2021-11-26 15:28:56 +0900

    소스 위치 변경 및 버그픽스
    1. 소괄호, 중괄호 오탈자 수정
    2. 구조체 종속변수 잘못된 이름 사용 수정

--- Auto_complete/Auto_complete/connection.c
+++ Auto_complete/Auto_complete/connection.c
@@ -1,29 +1,34 @@
+#include "dictionary.h"
+
 // 현재 문자열의 dictionary index 반환
 int searchWL(wordList* wl, char* string)
 {
-	for(int idx=0; idx<wl->count; idx++) {
-		if(!(strcmp(wl->stringList[idx], string))
+	for (int idx = 0; idx < wl->count; idx++) {
+		if (!(strcmp(wl->stringList[idx], string)))
 			return idx;
 
-	return -1;
+		return -1;
+	}
 }
 
 // 지정한 문자열에 대한 입력한 문자열의 connection count 반환
 int connectCount(Dictionary* dic, int dic_idx, char* string)
 {
-	for(int i=0; i<dic->wdic[dic_idx].count; i++) {
-		if(!(strcmp(dic->wdic[dic_idx].voc[i].string, string) {
-			return dic->wdic[dic_idx].voc[i].count;
-
-	return -1;
+	for (int i = 0; i < dic->wdic[dic_idx].count; i++) {
+		if (!(strcmp(dic->wdic[dic_idx].voc[i].string, string))) {
+			return dic->wdic[dic_idx].voc[i].connect;
+		}
+		return -1;
+	}
 }
 
 // 지정한 문자열에 대한 입력한 문자열의 connection string 반환
 char* connectString(Dictionary* dic, int dic_idx, char* string)
 {
-	for(int i=0; i<dic->wdic[dic_idx].count; i++) {
-		if(!(strcmp(dic->wdic[dic_idx].voc[i].string, string) {
+	for (int i = 0; i < dic->wdic[dic_idx].count; i++) {
+		if (!(strcmp(dic->wdic[dic_idx].voc[i].string, string))) {
 			return dic->wdic[dic_idx].voc[i].string;
-
+		}
+	}
 	return NULL;
 }
